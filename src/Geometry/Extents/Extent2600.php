e72d94243a38b8a5637db621e15968a9e0213b65
Doug Wright <github@dougweb.org>
2021-04-14 20:26:25 +0100

    Add polygons

---
+++ src/Geometry/Extents/Extent2600.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\Geometry\Extents;
+
+/**
+ * Africa/Algeria - Hassi Bir Reikaz.
+ * @internal
+ */
+class Extent2600
+{
+    public function __invoke(): array
+    {
+        return
+        [
+            [
+                [
+                    [7.166667, 31.75], [7.4583335, 31.75], [7.75, 31.75], [7.75, 31.916667], [8, 31.916667], [8, 32.166667], [8, 32.416667], [7.7222223333333, 32.416667], [7.4444446666667, 32.416667], [7.166667, 32.416667], [7.166667, 32.194444666667], [7.166667, 31.972222333333], [7.166667, 31.75],
+                ],
+            ],
+        ];
+    }
+}
---
+++ src/Geometry/Extents/Extent2785.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\Geometry\Extents;
+
+/**
+ * Africa/Libya - Murzuq and En Naga.
+ * @internal
+ */
+class Extent2785
+{
+    public function __invoke(): array
+    {
+        return
+        [
+            [
+                [
+                    [18.56307950344, 27.594378560249], [18.452829371471, 27.660677626094], [18.370141772495, 27.563091360636], [18.640552569147, 27.327692430217], [18.717280701531, 27.428258428972], [18.635338035879, 27.536273760968], [18.56307950344, 27.594378560249],
+                ],
+            ],
+        ];
+    }
+}
