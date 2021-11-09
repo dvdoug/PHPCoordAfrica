ecea08ff8a1217d34ad7eeab09d3df7d72db148c
Doug Wright <github@dougweb.org>
2021-11-09 21:45:34 +0000

    Add South Africa geoid

---
+++ src/CoordinateOperation/DATITRF2005SALLDProvider.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class DATITRF2005SALLDProvider implements GridProvider
+{
+    public function provideGrid(): DATHeightGrid
+    {
+        return new DATHeightGrid(__DIR__ . '/../../resources/SAGEOID2010.dat');
+    }
+}
